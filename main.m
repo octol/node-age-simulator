c1205c982c8d23f9e7642311e6bfc65171eaeb70
Jon Haggblad <jon.haggblad@gmail.com>
2019-01-11 10:35:54 +0100

    Rewrite to vectorize and remove array resizing
    Speedup seems to be 10-100x faster, making more realistic scenarious
    tractable.

--- main.m
+++ main.m
@@ -10,100 +10,76 @@ function index = random_index(indices)
     index = indices(randi([1, length(indices)]));
 end
 
-number_of_sections = 100;
+network_iterations = 1000;
+number_of_sections = 10000;
 min_section_size = 8;
-start_section_size = 12;
+start_section_size = 8;
+max_section_size = 20;
 total_start_nodes = start_section_size * number_of_sections;
 total_nodes = total_start_nodes;
 total_bad_nodes = 0;
 
 % Initialise network with min_section_size everywhere and only honest nodes
 % with zero age
-for section_ii = 1:number_of_sections
-    for node_ii = 1:start_section_size
-        section(section_ii).node(node_ii).work = 0;
-        section(section_ii).node(node_ii).age = 0;
-        section(section_ii).node(node_ii).durability = randi([1, 3]);
-        section(section_ii).node(node_ii).evil = 0;
-    end
-end
+nodes.work = zeros(number_of_sections, max_section_size);
+nodes.age = zeros(number_of_sections, max_section_size);
+nodes.durability = randi([1,3], number_of_sections, max_section_size);
+nodes.evil = zeros(number_of_sections, max_section_size);
+nodes.active = logical(zeros(number_of_sections, max_section_size));
+nodes.active(:,1:start_section_size) = ones(number_of_sections, start_section_size);
 
 % Evolve network before starting
-N = 1000;
-for n = 1:N
-    % Spread out total_nodes/3 amount of work
-    % Generate total_nodes number of index tuples (section_ii, node_ii)
-    total_work = total_nodes/3;
-    for ii = 1:total_work
-        section_ii = randi([1, number_of_sections]);
-        node_ii = randi([1, length(section(section_ii).node)]);
+for n = 1:network_iterations
+    % Part of the network does work
+    nodes_that_does_work = logical(randi([0,1], number_of_sections, max_section_size));
+    nodes.work(and(nodes_that_does_work, nodes.active)) += 1;
 
-        section(section_ii).node(node_ii).work += 1;
+    % Increase age and relocate if work == 2^n
+    nodes_to_age = rem(log2(nodes.work), 1) == 0;
+    nodes.age(nodes_to_age) += 1;
+    nodes_to_age_indices = find(nodes_to_age);
 
-        % Increase age and relocate if work == 2^N
-        log2_value = log2(section(section_ii).node(node_ii).work);
-        if abs(rem(log2_value, 1)) < 1e3*eps
-            section(section_ii).node(node_ii).age += 1;
+    % Find target slots
+    % Note: this means we relocate towards emptier section
+    node_slots_available = find(nodes.active == false);
+    assert(length(nodes_to_age_indices) < length(node_slots_available));
+    I = randperm(length(node_slots_available));
+    I = I(1:length(nodes_to_age_indices));
 
-            % Target index should be a among the smallest sections
-            section_sizes = get_sizes(section);
+    % Relocate
+    nodes.work(node_slots_available(I)) = nodes.work(nodes_to_age_indices);
+    nodes.age(node_slots_available(I)) = nodes.age(nodes_to_age_indices);
+    nodes.durability(node_slots_available(I)) = nodes.durability(nodes_to_age_indices);
+    nodes.evil(node_slots_available(I)) = nodes.evil(nodes_to_age_indices);
+    nodes.active(node_slots_available(I)) = nodes.active(nodes_to_age_indices);
+    nodes.work(nodes_to_age_indices) = 0;
+    nodes.age(nodes_to_age_indices) = 0;
+    nodes.durability(nodes_to_age_indices) = 0;
+    nodes.evil(nodes_to_age_indices) = 0;
+    nodes.active(nodes_to_age_indices) = 0;
 
-            % Alt 1: random among section below min_section_size
-            small_section_indices = find(section_sizes < min_section_size);
-            % Alt 2: random among the smallest sections
-            %minimal_section_size = min(section_sizes);
-            %small_section_indices = find(section_sizes == minimal_section_size);
-
-            if length(small_section_indices) > 0
-                target_ii = random_index(small_section_indices);
-            else
-                target_ii = randi([1, number_of_sections]);
-            end
-            if target_ii != section_ii
-                section(target_ii).node = [section(target_ii).node section(section_ii).node(node_ii)];
-                section(section_ii).node(node_ii) = [];
-            end
-        end
+    % Join new nodes
+    node_slots_available = find(nodes.active == false);
+    I = randperm(length(node_slots_available));
+    I = I(1:end/8);
+    nodes.work(node_slots_available(I)) = 0;
+    nodes.age(node_slots_available(I)) = 0;
+    nodes.durability(node_slots_available(I)) = randi([1,3], length(I), 1);
+    if n > 100
+        nodes.evil(node_slots_available(I)) = logical(rand(length(I), 1) < 0.1);
     end
-
-    % Randomly join new nodes
-    if total_nodes < total_start_nodes
-        new_nodes = total_start_nodes - total_nodes;
-        for ii = 1:new_nodes
-            section_ii = randi([1, number_of_sections]);
-            node_ii = length(section(section_ii).node) + 1;
-
-            evil = (n > 20) * (rand() < 0.1);
-            total_bad_nodes += evil;
-
-            section(section_ii).node(node_ii).work = 0;
-            section(section_ii).node(node_ii).age = 0;
-            section(section_ii).node(node_ii).durability = randi([1, 3]);
-            section(section_ii).node(node_ii).evil = evil;
-        end
-        total_nodes += new_nodes;
-    end
+    nodes.active(node_slots_available(I)) = true;
 
     % Randomly drop nodes depending on durability
-    for ii = 1:total_nodes/10
-        section_ii = randi([1, number_of_sections]);
-        node_ii = randi([1, length(section(section_ii).node)]);
+    nodes_to_drop = ((rand(number_of_sections, max_section_size).*nodes.active) ./ nodes.durability) > 0.3;
+    nodes.work(nodes_to_drop) = 0;
+    nodes.age(nodes_to_drop) = 0;
+    nodes.durability(nodes_to_drop) = 0;
+    nodes.evil(nodes_to_drop) = 0;
+    nodes.active(nodes_to_drop) = false;
 
-        chance_to_drop = rand()/section(section_ii).node(node_ii).durability;
-        if chance_to_drop > 0.5
-            total_bad_nodes -= section(section_ii).node(node_ii).evil;
-            total_nodes -= 1;
-            section(section_ii).node(node_ii) = [];
-            assert(length(section(section_ii).node) > 0, 'section size dropped to zero!')
-        end
-    end
+    section_spread = sum(nodes.active, 2)';
+    section_load = sum(nodes.evil, 2)';
 
-    % Max evil section load
-    for ii = 1:length(section)
-        section_size = length(section(ii).node);
-        s = sum([section(ii).node.evil]);
-        section_load(ii) = s/section_size;
-    end
-
-    fprintf('total_nodes: %d, total_bad_nodes: %d, max(section_load): %d\n', total_nodes, total_bad_nodes, max(section_load));
+    fprintf('total_nodes: %d, total_bad_nodes: %d, max(section_load): %d\n', sum(section_spread), sum(section_load), max(section_load));
 end
