1f711d2b7bfb6594eefac0b09517ea90218d0a33
Jon Haggblad <jon.haggblad@gmail.com>
2019-01-22 21:50:53 +0100

    Implement elders in section model

--- model_with_sections.m
+++ model_with_sections.m
@@ -47,6 +47,7 @@ nodes.age = zeros(number_of_sections, max_section_size);
 nodes.malicious = zeros(number_of_sections, max_section_size);
 nodes.active = logical(zeros(number_of_sections, max_section_size));
 nodes.active(:,1:start_section_size) = ones(number_of_sections, start_section_size);
+nodes.elder = logical(zeros(number_of_sections, max_section_size));
 
 nodes_active_indices = find(nodes.active);
 num_of_age_buckets = 16;
@@ -56,6 +57,15 @@ age_bucket_size = numel(nodes_active_indices) / num_of_age_buckets;
 % Start nodes at age 4 (they're adults, not infants)
 nodes.work(nodes_active_indices) = round(2.^((initial_network_age-4)*rand(size(nodes_active_indices)) + 4));
 nodes.age(nodes.active) = floor(log2(nodes.work(nodes.active)));
+
+assert(size(nodes.work,1) == number_of_sections);
+for s = 1:size(nodes.work,1)
+    [sorted_work,I] = sort(nodes.work(s,:),'descend');
+    nodes.elder(s,I(1:min_section_size)) = true;
+    if numel(I) > min_section_size
+        nodes.elder(s,I(min_section_size+1:end)) = false;
+    end
+end
 
 % Evolve network before starting
 for n = 1:network_iterations
@@ -133,16 +143,31 @@ for n = 1:network_iterations
     nodes.active(node_slots_available(I)) = true;
     assert(floor(log2(nodes.work(nodes.active))) == nodes.age(nodes.active));
 
+    % Assign elder status (slow!)
+    for s = 1:size(nodes.work,1)
+        [sorted_work,I] = sort(nodes.work(s,:),'descend');
+        nodes.elder(s,I(1:min_section_size)) = true;
+        if numel(I) > min_section_size
+            nodes.elder(s,I(min_section_size+1:end)) = false;
+        end
+    end
+    assert(all(sum(nodes.elder,2) == min_section_size))
+
     % Collect statistics
     section_size = sum(nodes.active, 2)';
     section_size_mean(n) = mean(section_size);
     section_size_std(n) = std(section_size);
-    network_size(n) = sum(section_size);
+    %network_size(n) = sum(section_size);
 
     section_size_malicious = sum(nodes.malicious, 2)';
-    section_size_malicious_mean(n) = mean(section_size_malicious);
-    section_size_malicious_std(n) = std(section_size_malicious);
-    netsize_size_malicious(n) = sum(section_size_malicious);
+    %section_size_malicious_mean(n) = mean(section_size_malicious);
+    %section_size_malicious_std(n) = std(section_size_malicious);
+    %network_size_malicious(n) = sum(section_size_malicious);
+
+    section_elders_malicious = sum(nodes.elder.*nodes.malicious,2)';
+    section_elders_malicious_mean(n) = mean(section_elders_malicious);
+    section_elders_malicious_mean(n) = mean(section_elders_malicious);
+    network_elders_malicious(n) = sum(section_elders_malicious);
 
     section_work = sum(nodes.work, 2)';
     section_work_mean(n) = mean(section_work);
@@ -159,12 +184,19 @@ for n = 1:network_iterations
     section_load_mean(n) = mean(section_load);
     section_load_std(n) = std(section_load);
 
+    section_elders_load = section_elders_malicious ./ section_size;
+    section_elders_load_mean(n) = mean(section_elders_load);
+    section_elders_load_std(n) = std(section_elders_load);
+
     section_work_load = section_work_malicious ./ section_work;
     section_work_load_mean(n) = mean(section_work_load);
     section_work_load_std(n) = std(section_work_load);
 
     stalled_sections(n) = sum(section_load > section_stalled_threshold) / number_of_sections;
     compromised_sections(n) = sum(section_load > section_compromised_threshold) / number_of_sections;
+
+    stalled_sections_elders(n) = sum(section_elders_load > section_stalled_threshold) / number_of_sections;
+    compromised_sections_edlers(n) = sum(section_elders_load > section_compromised_threshold) / number_of_sections;
 
     stalled_sections_work(n) = sum(section_work_load > section_stalled_threshold) / number_of_sections;
     compromised_sections_work(n) = sum(section_work_load > section_compromised_threshold) / number_of_sections;
@@ -174,18 +206,18 @@ for n = 1:network_iterations
         fprintf('  section work (mean/std): %d / %d \n', section_work_mean(n), section_work_std(n));
 
         figure(1)
-        subplot(2,2,1)
-        hist(nodes.age(nodes.active), 50);
-        xlabel('Age');
-        title(['Sections (number/mean/std/min): ',...
-            num2str(number_of_sections),'/',...
-            num2str(section_size_mean(n)),'/',...
-            num2str(section_size_std(n),2),'/',...
-            num2str(min_section_size)]
-        )
-        drawnow;
+        %subplot(2,2,1)
+        %hist(nodes.age(nodes.active), 50);
+        %xlabel('Age');
+        %title(['Sections (number/mean/std/min): ',...
+        %    num2str(number_of_sections),'/',...
+        %    num2str(section_size_mean(n)),'/',...
+        %    num2str(section_size_std(n),2),'/',...
+        %    num2str(min_section_size)]
+        %)
+        %drawnow;
 
-        subplot(2,2,2)
+        subplot(2,2,1)
         hold on
         H1 = plot(1:n, section_load_mean, 'LineWidth', 2, 'Color', 'k');
         H2 = plot(
@@ -199,6 +231,22 @@ for n = 1:network_iterations
         hold off
         legend([H1, H2(1), H3(1)], '\mu', '0.5\sigma', '\sigma', 'Location', 'Northwest');
         title('Malicious nodes per section')
+        drawnow;
+
+        subplot(2,2,2)
+        hold on
+        H1 = plot(1:n, section_elders_load_mean, 'LineWidth', 2, 'Color', 'k');
+        H2 = plot(
+            1:n, [max(section_elders_load_mean - 0.5*section_elders_load_std,0); section_elders_load_mean + 0.5*section_elders_load_std],
+            'LineWidth', 2, 'Color', 'b'
+        );
+        H3 = plot(
+            1:n, [max(section_elders_load_mean - section_elders_load_std,0); section_elders_load_mean + section_elders_load_std],
+            'LineWidth', 2, 'Color', 'm'
+        );
+        hold off
+        legend([H1, H2(1), H3(1)], '\mu', '0.5\sigma', '\sigma', 'Location', 'Northwest');
+        title('Malicious elders per section')
         drawnow;
 
         subplot(2,2,3)
@@ -218,8 +266,17 @@ for n = 1:network_iterations
         drawnow;
 
         subplot(2,2,4)
-        plot(1:n, stalled_sections, 'LineWidth', 2, 1:n, stalled_sections_work, 'LineWidth', 2);
-        legend('Stallable section (nodes)', 'Stallable sections (work)','Location','Northwest');
+        plot(
+            1:n, stalled_sections, 'LineWidth', 2,
+            1:n, stalled_sections_elders, 'LineWidth', 2,
+            1:n, stalled_sections_work, 'LineWidth', 2
+        );
+        legend(
+            'Stallable sections (nodes)',
+            'Stallable sections (elders)',
+            'Stallable sections (work)',
+            'Location','Northwest'
+        );
         title(['Adversary: ', num2str(fraction_of_new_nodes_are_malicious)]);
         drawnow
     end
