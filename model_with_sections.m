8bb2aa80025fcc0e93e73b069debf020651f2546
Jon Haggblad <jon.haggblad@gmail.com>
2019-01-22 13:20:38 +0100

    Correctly initialise section model

--- model_with_sections.m
+++ model_with_sections.m
@@ -1,17 +1,16 @@
 clear all
-figure(2); clf
-figure(3); clf
 
 network_iterations = 30000;
-init_iterations = 10000;
+init_iterations = 0;
 
 number_of_sections = 1000;
+initial_network_age = 16;
 
 % Scenario A
-%start_section_size = 16;
-%max_section_size = 40;
-%min_section_size = 8;
-%fraction_of_new_nodes_are_malicious = 0.10;
+start_section_size = 16;
+max_section_size = 40;
+min_section_size = 8;
+fraction_of_new_nodes_are_malicious = 0.20;
 
 % Scenario B
 %start_section_size = 32;
@@ -26,10 +25,17 @@ number_of_sections = 1000;
 %fraction_of_new_nodes_are_malicious = 0.20;
 
 % Scenario D
-start_section_size = 32;
-max_section_size = 80;
-min_section_size = 16;
-fraction_of_new_nodes_are_malicious = 0.20;
+%start_section_size = 32;
+%max_section_size = 80;
+%min_section_size = 16;
+%fraction_of_new_nodes_are_malicious = 0.20;
+
+% Scenario E
+%init_iterations = 0;
+%start_section_size = 128;
+%max_section_size = 320;
+%min_section_size = 64;
+%fraction_of_new_nodes_are_malicious = 0.25;
 
 section_stalled_threshold = 1/3;
 section_compromised_threshold = 2/3;
@@ -43,16 +49,13 @@ nodes.active = logical(zeros(number_of_sections, max_section_size));
 nodes.active(:,1:start_section_size) = ones(number_of_sections, start_section_size);
 
 nodes_active_indices = find(nodes.active);
-num_of_age_buckets = 8;
+num_of_age_buckets = 16;
 age_bucket_size = numel(nodes_active_indices) / num_of_age_buckets;
 
 % Flat spread of ages
 % Start nodes at age 4 (they're adults, not infants)
-for ii = 0:num_of_age_buckets-1
-    nodes.work(nodes_active_indices(ii*age_bucket_size+1:(ii+1)*age_bucket_size)) = 2^(2+ii);
-end
-node_age = log2(nodes.work);
-nodes.age(nodes.active) = node_age(nodes.active);
+nodes.work(nodes_active_indices) = round(2.^((initial_network_age-4)*rand(size(nodes_active_indices)) + 4));
+nodes.age(nodes.active) = floor(log2(nodes.work(nodes.active)));
 
 % Evolve network before starting
 for n = 1:network_iterations
@@ -104,7 +107,7 @@ for n = 1:network_iterations
         i = find(small_sections, 1);
         j = find(nodes.active(i,:) == false, 1);
         nodes.work(i,j) = 2^4;
-        nodes.age(i,j) = log2(nodes.work(i,j));
+        nodes.age(i,j) = round(log2(nodes.work(i,j)));
         if n > init_iterations
             nodes.malicious(i,j) = logical(rand() < fraction_of_new_nodes_are_malicious);
         else
@@ -121,13 +124,14 @@ for n = 1:network_iterations
     assert(length(I) > nodes_to_add);
     I = I(1:nodes_to_add);
     nodes.work(node_slots_available(I)) = 2^4;
-    nodes.age(node_slots_available(I)) = log2(2^4);
+    nodes.age(node_slots_available(I)) = round(log2(2^4));
     if n > init_iterations
         nodes.malicious(node_slots_available(I)) = logical(rand(length(I), 1) < fraction_of_new_nodes_are_malicious);
     else
         nodes.malicious(node_slots_available(I)) = false;
     end
     nodes.active(node_slots_available(I)) = true;
+    assert(floor(log2(nodes.work(nodes.active))) == nodes.age(nodes.active));
 
     % Collect statistics
     section_size = sum(nodes.active, 2)';
@@ -170,42 +174,56 @@ for n = 1:network_iterations
         fprintf('  section work (mean/std): %d / %d \n', section_work_mean(n), section_work_std(n));
 
         figure(1)
+        subplot(2,2,1)
         hist(nodes.age(nodes.active), 50);
         xlabel('Age');
+        title(['Sections (number/mean/std/min): ',...
+            num2str(number_of_sections),'/',...
+            num2str(section_size_mean(n)),'/',...
+            num2str(section_size_std(n),2),'/',...
+            num2str(min_section_size)]
+        )
         drawnow;
 
-        figure(2)
+        subplot(2,2,2)
         hold on
         H1 = plot(1:n, section_load_mean, 'LineWidth', 2, 'Color', 'k');
-        H2 = plot(1:n, [section_load_mean - 0.5*section_load_std; section_load_mean + 0.5*section_load_std], 'LineWidth', 2, 'Color', 'b');
-        H3 = plot(1:n, [section_load_mean - section_load_std; section_load_mean + section_load_std], 'LineWidth', 2, 'Color', 'm');
+        H2 = plot(
+            1:n, [max(section_load_mean - 0.5*section_load_std, 0); section_load_mean + 0.5*section_load_std],
+            'LineWidth', 2, 'Color', 'b'
+        );
+        H3 = plot(
+            1:n, [max(section_load_mean - section_load_std, 0); section_load_mean + section_load_std],
+            'LineWidth', 2, 'Color', 'm'
+        );
         hold off
         legend([H1, H2(1), H3(1)], '\mu', '0.5\sigma', '\sigma', 'Location', 'Northwest');
         title('Malicious nodes per section')
         drawnow;
 
-        figure(3)
+        subplot(2,2,3)
         hold on
         H1 = plot(1:n, section_work_load_mean, 'LineWidth', 2, 'Color', 'k');
-        H2 = plot(1:n, [section_work_load_mean - 0.5*section_work_load_std; section_work_load_mean + 0.5*section_work_load_std], 'LineWidth', 2, 'Color', 'b');
-        H3 = plot(1:n, [section_work_load_mean - section_work_load_std; section_work_load_mean + section_work_load_std], 'LineWidth', 2, 'Color', 'm');
+        H2 = plot(
+            1:n, [max(section_work_load_mean - 0.5*section_work_load_std,0); section_work_load_mean + 0.5*section_work_load_std],
+            'LineWidth', 2, 'Color', 'b'
+        );
+        H3 = plot(
+            1:n, [max(section_work_load_mean - section_work_load_std,0); section_work_load_mean + section_work_load_std],
+            'LineWidth', 2, 'Color', 'm'
+        );
         hold off
         legend([H1, H2(1), H3(1)], '\mu', '0.5\sigma', '\sigma', 'Location', 'Northwest');
         title('Malicious work per section')
         drawnow;
 
-        figure(4)
+        subplot(2,2,4)
         plot(1:n, stalled_sections, 'LineWidth', 2, 1:n, stalled_sections_work, 'LineWidth', 2);
-        legend('Stalled section (nodes)', 'Stalled sections (work)','Location','Northwest');
+        legend('Stallable section (nodes)', 'Stallable sections (work)','Location','Northwest');
+        title(['Adversary: ', num2str(fraction_of_new_nodes_are_malicious)]);
         drawnow
     end
 end
 
 %figure(1)
-%print -dpng section_model_age_distribution.png
-%figure(2)
-%print -dpng section_model_fraction_malicious_nodes.png
-%figure(3)
-%print -dpng section_model_fraction_malicious_work.png
-%figure(4)
-%print -dpng section_model_stalled_sections.png
+%print(["section_model_age_",num2str(initial_network_age), "_adversary_",num2str(fraction_of_new_nodes_are_malicious), "_section_size_", num2str(min_section_size),".png"],'-dpng');
