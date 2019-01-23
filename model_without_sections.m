6176a81a13e3213375b387c364ec71856f274442
Jon Haggblad <jon.haggblad@gmail.com>
2019-01-23 10:51:00 +0100

    Plot malicious age in sectionless model

--- model_without_sections.m
+++ model_without_sections.m
@@ -4,11 +4,11 @@ clear all
 init_network_iterations = 0;
 network_iterations = 20000;
 network_size = 100000;
-network_age = 16;
-fraction_of_new_nodes_are_malicious = 0.1;
+init_network_age = 16;
+fraction_of_new_nodes_are_malicious = 0.2;
 
 % Initial setup
-nodes.work = 2.^((network_age-4)*rand(1,network_size) + 4);
+nodes.work = 2.^((init_network_age-4)*rand(1,network_size) + 4);
 nodes.age = floor(log2(nodes.work));
 nodes.malicious = logical(zeros(size(nodes.work)));
 
@@ -30,26 +30,26 @@ for n = 1:init_network_iterations
         figure(1)
         hist(nodes.work, 50);
         xlabel('work');
-        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(network_age),', Iterations: ', num2str(n)])
+        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(init_network_age),', Iterations: ', num2str(n)])
         drawnow
 
         figure(2)
         hist(log2(nodes.work), 50);
         xlabel('log2(work)');
-        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(network_age),', Iterations: ', num2str(n)])
+        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(init_network_age),', Iterations: ', num2str(n)])
         drawnow
 
         figure(3)
         hist(nodes.age, 50);
         xlabel('age = floor(log2(work))')
-        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(network_age),', Iterations: ', num2str(n)])
+        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(init_network_age),', Iterations: ', num2str(n)])
         drawnow
 
         figure(4)
         semilogy(1:n, fraction_of_network_resetting, 1:n, fraction_of_work_resetting)
         legend("Network reset rate", "Work reset rate");
         grid on
-        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(network_age),', Iterations: ', num2str(n)])
+        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(init_network_age),', Iterations: ', num2str(n)])
         drawnow
     end
 end
@@ -83,8 +83,11 @@ for n = 1:network_iterations
 
     % Collect network work stats
     network_work = sum(nodes.work);
+    network_age = sum(nodes.age);
     malicious_work = sum(nodes.work(nodes.malicious));
+    malicious_age = sum(nodes.age(nodes.malicious));
     frac_malicious_work(init_network_iterations + n) = malicious_work / network_work;
+    frac_malicious_age(init_network_iterations + n) = malicious_age / network_age;
 
     fraction_of_nodes_are_malicious(init_network_iterations + n) = sum(nodes.malicious) / length(nodes.malicious);
 
@@ -118,24 +121,36 @@ for n = 1:network_iterations
         drawnow
 
         figure(4)
-        semilogy(1:length(fraction_of_network_resetting), fraction_of_network_resetting, 1:length(fraction_of_work_resetting), fraction_of_work_resetting)
-        legend({"Network reset rate", "Work reset rate"}, 'Location', 'West');
-        xlabel("work/node")
+        semilogy(
+            1:length(fraction_of_network_resetting), fraction_of_network_resetting,
+            1:length(fraction_of_work_resetting), fraction_of_work_resetting
+        )
+        legend({'Network reset rate', 'Work reset rate'}, 'Location', 'West');
+        xlabel('Iteration')
         grid on
-        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(network_age)])
+        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(init_network_age)])
         drawnow
 
         figure(5)
         N = 1:length(fraction_of_network_resetting);
-        plot(N, fraction_of_nodes_are_malicious, 'LineWidth', 2, N, frac_malicious_work, 'LineWidth', 2, N, frac_malicious_elder_work, 'LineWidth', 2);
-        xlabel("work/node")
-        legend({"Malicious nodes", "Malicious work", "Malicious elder work"},"Location", "NorthWest");
-        title(['Nodes: ',num2str(network_size),', Initial network age: ', num2str(network_age),' , Adversary: ', num2str(fraction_of_new_nodes_are_malicious)])
+        plot(
+            N, fraction_of_nodes_are_malicious, 'LineWidth', 2,
+            N, frac_malicious_age, 'LineWidth', 2,
+            N, frac_malicious_work, 'LineWidth', 2
+        );
+        xlabel('Iteration')
+        ylabel('Fraction')
+        legend({'Malicious nodes', 'Malicious age', 'Malicious work'}, 'Location', 'NorthWest');
+        title([
+            'Nodes: ',num2str(network_size),...
+            ', Initial network age: ', num2str(init_network_age),...
+            ' , Adversary: ', num2str(fraction_of_new_nodes_are_malicious)
+        ])
         drawnow
     end
 end
 
 %figure(4)
-%print(['simple_model_network_reset_rate_with_attack_network_age_',num2str(network_age),'_adversary_',num2str(fraction_of_new_nodes_are_malicious),'.png'],'-dpng')
+%print(['simple_model_network_reset_rate_with_attack_network_age_',num2str(init_network_age),'_adversary_',num2str(fraction_of_new_nodes_are_malicious),'.png'],'-dpng')
 %figure(5)
-%print(['simple_model_malicious_fractions_network_age_',num2str(network_age),'_adversary_',num2str(fraction_of_new_nodes_are_malicious),'.png'],'-dpng')
+%print(['simple_model_malicious_fractions_network_age_',num2str(init_network_age),'_adversary_',num2str(fraction_of_new_nodes_are_malicious),'.png'],'-dpng')
