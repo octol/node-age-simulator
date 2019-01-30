8b42bc921d0cfc036f85f8e852c4e95033595d0d
Jon Haggblad <jon.haggblad@gmail.com>
2019-01-30 18:35:06 +0100

    Simulation file for testing to fix elders

---
+++ section_model/sim_section_size_increase_fix_elders.m
@@ -0,0 +1,30 @@
+clear all
+
+network_iterations = 20000;
+init_iterations = 0;
+initial_network_age = 16;
+
+% Test increasing section size
+number_of_sections = [5000 2500 1000 500];
+start_section_size = [20 40 100 200];
+max_section_size = [50 100 250 500];
+min_section_size = [10 20 50 100];
+num_of_elders = [10 10 10 10];
+fraction_of_new_nodes_are_malicious = 0.20;
+
+for ii = 1:length(number_of_sections)
+    fprintf('Running with number_of_sections: %d\n', number_of_sections(ii));
+    [nodes{ii}, stats{ii}] = run_section_model(
+        number_of_sections(ii),
+        start_section_size(ii),
+        min_section_size(ii),
+        max_section_size(ii),
+        initial_network_age,
+        num_of_elders(ii),
+        network_iterations,
+        init_iterations,
+        fraction_of_new_nodes_are_malicious
+    );
+end
+
+save sim_section_size_increase_fix_elders.dat
