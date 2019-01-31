4defaf5234ccd7db369abb09e16717f36ac034db
Jon Haggblad <jon.haggblad@gmail.com>
2019-01-31 08:40:26 +0100

    Derive parameters from each other in sim_section

--- section_model/sim_section_size_increase.m
+++ section_model/sim_section_size_increase.m
@@ -5,10 +5,11 @@ init_iterations = 0;
 initial_network_age = 16;
 
 % Test increasing section size
-number_of_sections = [5000 2500 1000 500];
-start_section_size = [20 40 100 200];
-max_section_size = [50 100 250 500];
+network_size = 100000;
 min_section_size = [10 20 50 100];
+start_section_size = round(2*min_section_size)
+max_section_size = round(5*min_section_size)
+number_of_sections = network_size ./ start_section_size
 num_of_elders = min_section_size;
 fraction_of_new_nodes_are_malicious = 0.20;
 
@@ -27,4 +28,4 @@ for ii = 1:length(number_of_sections)
     );
 end
 
-save sim_section_size_increase.dat
+save('sim_section_size_increase.dat')
--- section_model/sim_section_size_increase_fix_elders.m
+++ section_model/sim_section_size_increase_fix_elders.m
@@ -5,12 +5,17 @@ init_iterations = 0;
 initial_network_age = 16;
 
 % Test increasing section size
-number_of_sections = [5000 2500 1000 500];
-start_section_size = [20 40 100 200];
-max_section_size = [50 100 250 500];
-min_section_size = [10 20 50 100];
-num_of_elders = [10 10 10 10];
+network_size = 100000;
+%min_section_size = [10 20 50 100];
+min_section_size = [20 50 100]
+start_section_size = round(2*min_section_size)
+max_section_size = round(5*min_section_size)
+number_of_sections = network_size ./ start_section_size
+num_of_elders = 17*ones(size(number_of_sections));
 fraction_of_new_nodes_are_malicious = 0.20;
+
+assert(all((network_size - number_of_sections(1)*start_section_size(1)) == 0))
+assert(all((number_of_sections.*start_section_size - network_size) == 0))
 
 for ii = 1:length(number_of_sections)
     fprintf('Running with number_of_sections: %d\n', number_of_sections(ii));
@@ -27,4 +32,4 @@ for ii = 1:length(number_of_sections)
     );
 end
 
-save sim_section_size_increase_fix_elders.dat
+save(['sim_section_size_increase_fix_elders_',num2str(num_of_elders(1)),'.dat'])
