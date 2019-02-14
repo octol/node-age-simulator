41cd09f6b89406234c8c40486b789cf5892a9ecc
Jon Haggblad <jon.haggblad@gmail.com>
2019-02-14 14:25:10 +0100

    Batch of results for zero churn fix elders

--- section_model/sim_section_size_increase_zero_churn_adv_fix_elders.m
+++ section_model/sim_section_size_increase_zero_churn_adv_fix_elders.m
@@ -2,17 +2,17 @@ clear all
 
 network_iterations = 20000;
 init_iterations = 0;
-initial_network_age = 16;
+initial_network_age = 12;
 
 % Test increasing section size
 network_size = 100000;
 %min_section_size = [10 20 50 100]
 min_section_size = [100]
-start_section_size = round(2*min_section_size)
-max_section_size = round(5*min_section_size)
-number_of_sections = network_size ./ start_section_size
+start_section_size = 2*min_section_size
+max_section_size = 5*min_section_size
+number_of_sections = ceil(network_size ./ start_section_size)
 num_of_elders = 10*ones(size(number_of_sections))
-fraction_of_new_nodes_are_malicious = 0.05;
+fraction_of_new_nodes_are_malicious = 0.05
 zero_churn_adversary = true;
 
 for ii = 1:length(number_of_sections)
